9b29fa0596ba27bfb4df2594817d2af3d8f34b91
Alex Muñoz <lomejor77@gmail.com>
2023-06-30 10:47:29 -0400

    Ejercicio 12 agregado binding y List en el segundo activity

--- app/src/main/java/cl/awakelabs/ejercicio12/Activity_Platos.java
+++ app/src/main/java/cl/awakelabs/ejercicio12/Activity_Platos.java
@@ -1,14 +1,34 @@
 package cl.awakelabs.ejercicio12;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.awakelabs.ejercicio12.databinding.ActivityMainBinding;
+import cl.awakelabs.ejercicio12.databinding.ActivityPlatosBinding;
 
 public class Activity_Platos extends AppCompatActivity {
+    private  ActivityPlatosBinding  binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_platos);
+        binding = ActivityPlatosBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        List<String> listMenu = new ArrayList<>();
+
+        listMenu.add("Combo Hamburguesa");
+        listMenu.add("Combo Completos");
+        listMenu.add("Colacion Ejecutivo");
+        listMenu.add("Pollo con Papas Fritas");
+        listMenu.add("Asado a la olla con puré");
     }
 }
--- app/src/main/java/cl/awakelabs/ejercicio12/MainActivity.java
+++ app/src/main/java/cl/awakelabs/ejercicio12/MainActivity.java
@@ -2,13 +2,33 @@ package cl.awakelabs.ejercicio12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import cl.awakelabs.ejercicio12.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent menu = new Intent(getBaseContext(),Activity_Platos.class);
+                startActivity(menu);
+            }
+        });
+
+
     }
 }
