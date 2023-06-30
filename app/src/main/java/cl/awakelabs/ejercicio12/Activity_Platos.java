3aba61b1fdebc11b658b381edaae91447b3b1b29
Alex Muñoz <lomejor77@gmail.com>
2023-06-30 10:14:48 -0400

    Ejercicio 12 agregado dos pantallas y listview

---
+++ app/src/main/java/cl/awakelabs/ejercicio12/Activity_Platos.java
@@ -0,0 +1,14 @@
+package cl.awakelabs.ejercicio12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Platos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_platos);
+    }
+}
